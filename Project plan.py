3a030f47e5fd55944040b6cf9856182af5a27fad
shakila <shakila.jafari@go.minneapolis.edu>
2025-04-30 10:40:13 -0500

    Initial commit

---
+++ Project plan.py
@@ -0,0 +1,55 @@
+
+Author Name: Shakila Jafari
+
+Date: Aprile 23, 2025
+
+College Name: Minneapolis Community and Technical College
+
+
+
+Executive Summary:
+
+This app will be created to help customers to have more easy and simple access to recipes in no time just by choosing from the recipe list, making cooking a more pleasant experience. This cooking app which focuses more on egg recipes’ target audience are people who want to learn cooking and learning how to make eggs is the best start. This app is also a good start if you want to try new ways to make your eggs and do not want to dig too deep on the internet to find a good recipe that is worthwhile. The business value and impact of this app will be increased user engagement with a specific customer base interested in egg recipes. The possibility of revenue generation through app ads, subscription services, or affiliate partnerships with food and cooking products. The timeline for this app will be 4-5 weeks, and app testing might take 1-2 weeks.
+
+Functional Requirements:
+
+1: As a home cook, I want a filter to search for recipes by difficulty level, so that I can find dishes that match my cooking skill level.
+
+2: As someone who does
+n't have much time to cook, I want to see recipes labeled ‘quick and easy’ so that I can find recipes that are fast and convenient to prepare.
+
+3: As an egg lover, I want to filter recipes based on the type of egg used, so that I can easily find recipes using eggs that I like.
+
+4: As someone with dietary restrictions, I want to be able to filter recipes that are egg-free, so that I can avoid recipes that contain eggs in them.
+
+5: As a person who is new to cooking, I want access to a tutorial and ste p -b y -step instructions for basic egg recipes, so that I can learn how to.
+
+Technical Elements:
+
+I will be developing the program in Python using PyCharm with comments to explain what each function and code is doing.
+
+Variables and Data Types:
+
+The user will choose what recipe they want, and the app will allow them to enter their answer using input () function to ask the user for input and a text file, to keep the recipes.
+
+Mathematical Operations:
+
+I do
+n't use any mathematical operation in this app and it’s all text base.
+
+Boolean Operations and Conditions:
+
+In this app Boolean operation and conditionals will not be used.
+
+Loops:
+
+Loops will not be used in this app.
+
+Data Structures:
+
+The program will use text files to store the recipes. Each recipe will be in a different text file, so it will be easy to change if needed.
+
+Code Modularization:
+
+The program will be organized into functions including ‘display_recipe_name’, ‘get_user_input()’, ‘display_recipe’. Each function will perform a specific task.
+
